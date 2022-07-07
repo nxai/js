8dbf5db6c6c0f5027e35d665e6ffa0396db19860
lao2dev <nxai96@hotmail.com>
2022-07-07 14:33:32 +0700

    up

---
+++ dom_b.js
@@ -0,0 +1,6 @@
+const a = document.getElementsByTagName('p');
+console.log(a);
+const b = document.getElementById('demo');
+console.log(b);
+const c = document.getElementsByClassName('box');
+console.log(c);
